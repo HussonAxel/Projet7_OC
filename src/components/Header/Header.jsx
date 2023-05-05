c740c78caabf5175f7bb53b160736e4f7916d6fc
Husson <axel.husson@smile.fr>
2023-05-05 15:25:35 +0200

    enhance active navlink

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,10 +8,17 @@ export const Header = () => {
         <img src={logo} alt="logo" className="logo" />
       </NavLink>
       <ul className="listWrapper">
-        <NavLink to={"/"} activeclassname="active">
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          {" "}
           <li>Accueil</li>
         </NavLink>
-        <NavLink to={"/about"} activeclassname="active">
+        <NavLink
+          to={"/about"}
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
           <li>A Propos</li>
         </NavLink>
       </ul>
